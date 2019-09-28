95e28fb0b0e2b0027cea47db3e756ea2d08bc42a
rohan01939 <rohan01939@gmail.com>
2019-09-28 14:15:29 -0400

    datatypes,variables

---
+++ src/secondpackage/Abcd.java
@@ -0,0 +1,15 @@
+package secondpackage;
+
+public class Abcd {
+public static void main(String[] args) {
+	String ab="Rohan";
+	String age="25";
+	
+	
+System.out.println("my name is "+ab + " and I am "+ age +" old.");
+
+
+
+}
+}
+
